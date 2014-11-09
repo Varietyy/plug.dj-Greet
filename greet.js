25c2c54718349d76ddcb8936bd1e5b1cf611ec7b
Varietyy <chasedss@gmail.com>
2014-11-09 00:55:42 -0500

    Update greet.js

--- greet.js
+++ greet.js
@@ -4,22 +4,10 @@
  */
 
 API.chatLog("Variety's Greet Script Commands: /on and /off", true);
-API.on(API.CHAT_COMMAND, command);
 API.on(API.USER_JOIN, greet);
 
 function greet(data) {
    API.sendChat('Welcome! @' + data.username);
-}
-
-function command(value) {
-    if (value == "/on") {
-        API.on(API.USER_JOIN, greet);
-        API.chatLog("Greet [ON]", alert);
-    }
-    else if (value == "/off") {
-        API.off(API.USER_JOIN, greet);
-        API.chatLog("Greet [OFF]", alert);
-    }
 }
 
 function BassPlugLite(){
