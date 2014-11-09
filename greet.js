823243ccb7384adf72d7cbc60e0445bf24082082
Varietyy <chasedss@gmail.com>
2014-11-08 23:24:11 -0500

    Update greet.js

--- greet.js
+++ greet.js
@@ -1,9 +1,14 @@
+/*
+ * Original Author of the Autowoot code
+ * @derpthebass (Caleb)
+ */
+
 API.chatLog("Variety's Greet Script Commands: /on and /off", true);
 API.on(API.CHAT_COMMAND, command);
 API.on(API.USER_JOIN, greet, this);
 
 function greet(data) {
-   API.sendChat('Welcome! @' + data.un);
+   setTimeout(function(){API.sendChat('Welcome! @' + data.username);}, 5000);
 }
 
 function command(value) {
@@ -16,3 +21,23 @@ function command(value) {
         API.chatLog("Greet [OFF]", alert);
     }
 }
+
+function BassPlugLite(){
+bpl = {
+    autowoot: true,
+    clicks: 0,
+    version: 3.05,
+    close: function(){ API.off(API.DJ_ADVANCE, bpl.djAdvance); API.off(API.CHAT, bpl.chat); $('#woot').unbind('click', bpl.doubleClick); },
+    djAdvance: function() { if (bpl.autowoot) { setTimeout(function(){ $("#woot").click() }, 2000); }},
+    doubleClick: function() { bpl.clicks++; if (bpl.clicks == 2) { bpl.autowoot = !bpl.autowoot; bpl.clicks = 0; API.chatLog(bpl.autowoot ? 'AutoWoot is now on' : 'AutoWoot is now off'); } setTimeout(function() { bpl.clicks = 0 }, 600)}
+    }
+    
+    API.on(API.ADVANCE, bpl.djAdvance, this);
+    $("#woot").bind('click', bpl.doubleClick); 
+        
+    $('#woot').click();
+}
+
+if(typeof bpl !== "undefined") bpl.close();
+
+BassPlugLite();
